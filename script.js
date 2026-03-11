1280c147eb258c903120cb78de4758c65f2270e0
viktor Maly <viktormaly01@gmail.com>
2026-03-11 22:11:00 +0000

    Melhorei a lógica da calculadora para guardar os números

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 buttons = document.querySelectorAll("button")
+operador = document.querySelectorAll(".operador")
 outputResult = document.querySelector(".output-result")
 
-console.log(outputResult)
+operar = false
 
-num1 = 1
-operator = "+"
-num2 = 2
-
+num1 = ""
+num2 = ""
 
 function add(){
    return num1 + num2
@@ -24,17 +23,43 @@ function divide(){
     return num1 - num2
 }
 
-
+//logica
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        const element = event.target;
+        if(element.classList.contains("operator")){
+            operar = button.textContent
+        }
+    });
+});
 
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
+    button.addEventListener("click", (event) => {     
+        const element = event.target;
+
+        if(!operar){
+            num1 += button.textContent
+            console.log(num1)
+        }else if(element.classList.contains("btn-value")){
+            num2 += button.textContent
+            console.log(num2)
+        }
+            
         outputResult.innerText += button.textContent
+
+        if(button.textContent == "="){
+            operate(operar, num1, num2)
+            console.log(operate((operar, num1, num2)))
+        }
     })
 });
 
-
 function operate(operator, num1, num2){
-    return num1 + operator + num2
+    resultado = num1 + operator + num2
 }
 
 console.log(operate(add(), 1, 2))
+
+valor1 = 1
+valor2 = 2
+valor3 = valor1+valor2
