74b5214dbdf8d777a4d530451774ec2d5cbbdc5c
viktor Maly <viktormaly01@gmail.com>
2026-03-11 17:26:31 +0000

    Adicionei o evento de clique para a calculadora e implementei a atualização dos números no display

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+buttons = document.querySelectorAll("button")
+outputResult = document.querySelector(".output-result")
+
+console.log(outputResult)
+
 num1 = 1
 operator = "+"
 num2 = 2
@@ -19,6 +24,13 @@ function divide(){
     return num1 - num2
 }
 
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        outputResult.innerText += button.textContent
+    })
+});
 
 
 function operate(operator, num1, num2){
